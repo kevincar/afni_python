438eac748d8cf5b6d7a5a1b86ed4832a28a3bab3
kevincar <kevincarrolldavis@gmail.com>
2021-11-05 00:10:29 -0400

    Enable qsub

--- gp_step3_submit.py
+++ gp_step3_submit.py
@@ -9,15 +9,17 @@ decon_type can be dmBLOCK, GAM, 2GAM, or TENT
 """
 
 import os
+import sys
 import time
 import json
 import fnmatch
 import subprocess
 
-from typing import List, Dict
+from typing import List, Dict, Optional
 from datetime import datetime
 from argparse import ArgumentParser, Namespace
 from gp_step1_preproc import print_process_output
+from q import qsub
 
 # set up
 # code_dir = "/home/nmuncy/compute/learn_mvpa"
@@ -34,18 +36,23 @@ def main():
 
     # Arguments for job submission
     parser: ArgumentParser = ArgumentParser(prog="gp_step3_submit")
-    parser.add_argument("code_dir", help="Path to code repository")
-    parser.add_argument("work_dir", help="Path to parent data directory")
-    parser.add_argument("decon_type", help="Type of deconvolution to perform")
-    parser.add_argument("decon_dict_path", help="Path to decon dict JSON")
+    parser.add_argument("-c", "--code", help="Path to code repository")
+    parser.add_argument("-p", "--parent", help="Path to parent data directory")
+    parser.add_argument("-t", "--type", help="Type of deconvolution to perform")
+    parser.add_argument("-d", "--decondict", help="Path to decon dict JSON")
+    parser.add_argument(
+        "-n", "--batchnum", type=int,
+        help="Number of subjects to include in a batch", default=10
+    )
     parser.add_argument("sess_list", nargs="+", help="List of sessions")
 
     # Gather arguments
     args: Namespace = parser.parse_args()
-    code_dir: str = args.code_dir
-    work_dir: str = args.work_dir
-    decon_type: str = args.decon_type
-    decon_dict_path: str = args.decon_dict_path
+    code_dir: str = args.code
+    work_dir: str = args.parent
+    decon_type: str = args.type
+    decon_dict_path: str = args.decondict
+    batch_size: int = args.batchnum
     sess_list: List[str] = args.sess_list
 
     # Load decon_dict
@@ -87,16 +94,23 @@ def main():
             run_list.append(subj)
 
     # make batch list
-    if len(run_list) > 10:
-        batch_list = run_list[0:10]
+    if len(run_list) > batch_size:
+        batch_list = run_list[0:batch_size]
     else:
         batch_list = run_list
 
     for subj in batch_list:
         for sess in sess_list:
 
+            subj_num: int = int(subj.split("-")[1])
+            job_name: str = f"GP3{subj_num}"
+            time_limit: str = "50:00:00"
+            memory: int = 4000
             h_out = os.path.join(out_dir, f"out_{subj}_{sess}.txt")
             h_err = os.path.join(out_dir, f"err_{subj}_{sess}.txt")
+            code_file_path: str = os.path.join(code_dir, "gp_step3_decon.py")
+            sess = sess if sess != "" else "\"\""
+            h_cmd: str = f"{sys.executable} {code_file_path} {subj} {sess} {decon_type} {deriv_dir} {decon_dict_path}"
 
             # It's the same for all subjects...
             # write decon_dict to json in subj dir
@@ -105,41 +119,39 @@ def main():
             # ) as outfile:
             # json.dump(decon_dict, outfile)
 
-            script_name: str = f"{code_dir}/gp_step3_decon.py"
-            sess = sess if sess != "" else "\"\""
-            py_cmd: str = f"""
-                python {script_name} \
-                        {subj} \
-                        {sess} \
-                        {decon_type} \
-                        {deriv_dir} \
-                        {decon_dict_path}"""
             command: str = f"""
                 module load python-3.7.0-gcc-8.2.0-joh2xyk \n \
-                {py_cmd}
+                {h_cmd}
                 """
 
-            sbatch_job = f"""
-                sbatch \
-                    -J "GP3{subj.split("-")[1]}" \
-                    -t 50:00:00 \
-                    --mem=4000 \
-                    --ntasks-per-node=1 \
-                    -p IB_44C_512G  \
-                    -o {h_out} -e {h_err} \
-                    --account iacc_madlab \
-                    --qos pq_madlab \
-                    --wrap="{command}"
-            """
-
-            if os.environ.get("SLURM") is not None:
-                sbatch_submit = subprocess.Popen(
-                    sbatch_job, shell=True, stdout=subprocess.PIPE
-                )
-                job_id = sbatch_submit.communicate()[0]
-                print(job_id.decode("utf-8"))
+            hpc: Optional[str] = os.environ.get("HPC")
+            if hpc is not None:
+                if hpc == "SLURM":
+                    sbatch_job = f"""
+                        sbatch \
+                            -J "{job_name} "\
+                            -t {time_limit} \
+                            --mem={memory} \
+                            --ntasks-per-node=1 \
+                            -p IB_44C_512G  \
+                            -o {h_out} -e {h_err} \
+                            --account iacc_madlab \
+                            --qos pq_madlab \
+                            --wrap="{command}"
+                    """
+                    sbatch_submit = subprocess.Popen(
+                        sbatch_job, shell=True, stdout=subprocess.PIPE
+                    )
+                    job_id = sbatch_submit.communicate()[0]
+                    print(job_id.decode("utf-8"))
+                elif hpc == "QSUB":
+                    project_name: str = os.environ.get("PROJECT_NAME", "")
+                    qsub(h_cmd, project_name=project_name, job_name=job_name,
+                         time_limit=time_limit, memory=int(memory // 1000),
+                         stdout_file=h_out, stderr_file=h_err
+                         )
             else:
-                proc = subprocess.Popen(py_cmd, shell=True, stdout=subprocess.PIPE)
+                proc = subprocess.Popen(h_cmd, shell=True, stdout=subprocess.PIPE)
                 print_process_output(proc)
                 proc.wait()
                 if proc.returncode != 0:
