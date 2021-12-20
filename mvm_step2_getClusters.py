5d30a2345e9e8660fec847ca75162e10c38ec9ac
kevincar <kevincarrolldavis@gmail.com>
2021-12-20 16:50:45 -0500

    Update cluserizer

--- mvm_step2_getClusters.py
+++ mvm_step2_getClusters.py
@@ -1,10 +1,13 @@
 # %%
 import os
+import json
 import math
 import subprocess
 
+from typing import Dict
+from argparse import ArgumentParser, Namespace
 
-# %%
+
 def func_getClusters(grp_dir, mvm_dict):
 
     # get K threshold from p < .001, NN=1, 2sided
@@ -30,7 +33,7 @@ def func_getClusters(grp_dir, mvm_dict):
                 -ithr {mvm_dict[comp][1]} \
                 -NN 1 \
                 -clust_nvox {thr_num} \
-                -bisided -3.4372 3.4372 \
+                -bisided p=0.001 \
                 -pref_map Clust_{comp} \
                 > Table_{comp}.txt
         """
@@ -39,13 +42,40 @@ def func_getClusters(grp_dir, mvm_dict):
             h_clust.wait()
 
 
-# %%
 def main():
 
+    # Setup argument
+    parser: ArgumentParser = ArgumentParser(prog="getClusers")
+    parser.add_argument("-g", "--group", required=True, help="Path to the group analysis directory")
+    parser.add_argument("-d", "--data", required=True, help="Path to the data directory")
+
+    # Capture arguments
+    args: Namespace = parser.parse_args()
+    group_dir: str = args.group
+    data_dir: str = args.data
+    docs_dir: str = os.path.join(data_dir, "docs")
+
+    # Create the mvm_dict using the glt_dict
+    glt_dict_filename: str = "glt_dict.json"
+    glt_dict_filepath: str = os.path.join(docs_dir, glt_dict_filename)
+
+    glt_dict: Dict
+    with open(glt_dict_filepath, "r") as fh:
+        glt_dict = json.load(fh)
+
+    start_brik: int = 4
+    cur_brik: int = start_brik
+    mvm_dict: Dict = {}
+    for comparison in glt_dict:
+        mvm_dict.update({
+            comparison: [cur_brik, cur_brik + 1]
+        })
+        cur_brik += 2
+
     # TODO update for multiple MVMs
-    grp_dir = "/scratch/madlab/emu_power/analyses"
-    mvm_dict = {"Hit-Miss": [2, 3], "CR-FA": [4, 5]}
-    func_getClusters(grp_dir, mvm_dict)
+    # grp_dir = "/scratch/madlab/emu_power/analyses"
+    # mvm_dict = {"Hit-Miss": [2, 3], "CR-FA": [4, 5]}
+    func_getClusters(group_dir, mvm_dict)
 
 
 if __name__ == "__main__":
